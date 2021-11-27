3a82f7ad0d60e4ca12d2bdb4732f937767a3a4ca
Paolo Ardoino <paolo.ardoino@gmail.com>
2021-11-27 20:26:40 +0100

    Revert "use pipe"

--- lib.js
+++ lib.js
@@ -1,15 +1,73 @@
 module.exports = {
   connHandler: (connection, _dst) => {
+    let locConnected = false
+    let remConnected = true
+
     const loc = _dst() 
 
-    loc.pipe(connection).pipe(loc)
+    loc.on('connect', err => {
+      locConnected = true
+    })
 
-    loc.on('error', unpipe).on('close', unpipe)
-    connection.on('error', unpipe).on('close', unpipe)
+    loc.on('error', err => {
+      console.error(err)
+      try {
+        loc.end()
+      } catch (e) {
+        console.error(e)
+      }
+    })
 
-    function unpipe () {
-      connection.destroy()
-      loc.destroy()
-    }
+    loc.on('data', d => {
+      try {
+        connection.write(d)
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    loc.on('close', x => {
+      locConnected = false
+
+      if (remConnected) {
+        try {
+          connection.end()
+        } catch (e) {
+          console.error(e)
+        }
+        remConnected = false
+      }
+    })
+
+    connection.on('error', err => {
+      try {
+        connection.end()
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    connection.on('data', d => {
+      if (!locConnected) {
+        return
+      }
+
+      try {
+        loc.write(d)
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    connection.on('close', x => {
+      if (locConnected) {
+        try {
+          loc.end()
+        } catch (e) {
+          console.error(e)
+        }
+        locConnected = false
+      } 
+    })
   }
 }

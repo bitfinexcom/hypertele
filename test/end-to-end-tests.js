a19f646deb7bda67c0344eae7bfc3dad3730d900
HDegroote <75906619+HDegroote@users.noreply.github.com>
2024-04-08 23:02:53 +0200

    Fix test: 1000ms timeout for the test with expected timeout

--- test/end-to-end-tests.js
+++ test/end-to-end-tests.js
@@ -34,7 +34,7 @@ test('Cannot access private-mode server with public key', async t => {
   const clientPort = await setupHyperteleClient(pubKey, bootstrap, t, { isPrivate: false })
 
   // Could also be a socket hangup if more time is given
-  await t.exception(async () => await request(clientPort), /Request timeout/)
+  await t.exception(async () => await request(clientPort, { msTimeout: 1000 }), /Request timeout/)
 })
 
 test('Can proxy in non-private mode', async t => {
